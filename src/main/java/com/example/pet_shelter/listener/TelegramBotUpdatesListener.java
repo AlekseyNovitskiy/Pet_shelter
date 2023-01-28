1a0e6e76c6f91206f7153e0c46edf5af61d29fee
PDemenkov <guapnik@gmail.com>
2023-01-28 18:12:15 +0300

    filereader/value added

--- src/main/java/com/example/pet_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/pet_shelter/listener/TelegramBotUpdatesListener.java
@@ -10,10 +10,13 @@ import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.awt.*;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 @Service
