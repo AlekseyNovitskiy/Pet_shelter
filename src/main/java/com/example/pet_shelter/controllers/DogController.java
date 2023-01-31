1aef0da78a48fd96db6c42ca6294bf49e475abc3
93294 <9329496@mail.ru>
2023-01-31 21:22:58 +0300

    Работа с фото питомцев

--- src/main/java/com/example/pet_shelter/controllers/DogController.java
+++ src/main/java/com/example/pet_shelter/controllers/DogController.java
@@ -1,50 +1,84 @@
 package com.example.pet_shelter.controllers;
 
 import com.example.pet_shelter.model.Dogs;
+import com.example.pet_shelter.model.DogsFoto;
 import com.example.pet_shelter.service.DogsFotoService;
 import com.example.pet_shelter.service.DogsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/dogs")
 public class DogController {
 
-    @Value ("${size.FotoDogs}")
+    @Value("${size.FotoDogs}")
     int SIZE_FOTO_DOG; // Максимальный размер файла
 
-    @Autowired
-    private DogsService dogsService;
+    private final DogsService dogsService;
+    private final DogsFotoService dogsFotoService;
 
-    @Autowired
-    private DogsFotoService dogsFotoService;
-
-    @PostMapping("/create")   // Создание новой записи о собаке
-    public Dogs createDog (@RequestBody Dogs dogs) {
-        return this.dogsService.createDog(dogs);
+    public DogController(DogsService dogsService, DogsFotoService dogsFotoService) {
+        this.dogsService = dogsService;
+        this.dogsFotoService = dogsFotoService;
     }
 
-    @PostMapping(value = "/{id}/fotoDog", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)  // Прикрепление фото собаки
-    public  ResponseEntity<String> uploadFotoDog(@PathVariable Long id, @RequestParam MultipartFile foto) throws IOException {
-        if (foto.getSize() >= SIZE_FOTO_DOG) {         // Ограничение размера файла
+    // Внесение данных о новой собаке
+    @PostMapping("/create")   // Создание новой записи о собаке
+    @Operation(summary = "Внесение данных о новой питомце")
+    public Dogs createDog(@RequestBody Dogs dog) {
+        return this.dogsService.createDog(dog);
+    }
+
+    // Удаление информации о собаке
+    @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Удаление данных о питомце")
+    public Dogs deleteDog(@PathVariable("id") Long id) {
+        return this.dogsService.deleteDog(id);
+    }
+
+    // Изменение информации о собаке
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Изменение данных о питомце")
+    public Dogs deleteDog(@PathVariable("id") Long id, @RequestBody Dogs dog) {
+        return this.dogsService.updateDog(id, dog);
+    }
+
+    // Загрузка фотографии собаки
+    @PostMapping(value = "/{id}/load/fotoDog", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Загрузка фотографии питомца")
+    // Прикрепление фото собаки
+    public ResponseEntity<String> uploadFotoDog(@PathVariable Long id, @RequestParam MultipartFile fotoDog) throws IOException {
+        if (fotoDog.getSize() >= SIZE_FOTO_DOG) {         // Ограничение размера файла
             return ResponseEntity.badRequest().body("Файл большого размера");
         }
-        dogsFotoService.uploadFotoDog(id,foto);
+        dogsFotoService.uploadFotoDog(id, fotoDog);
         return ResponseEntity.ok().build();
     }
 
-
-  //  @GetMapping(value = "/{id}/fotoDog") // Получение фото собаки по id
-   // public ResponseEntity<byte[]> downloadFotoDog(@PathVariable Long id) {
-
-  //  }
-
-
+    // Просмотр фотографии собаки
+    @GetMapping(value = "/{id}/fotoDog")
+    @Operation(summary = "Просмотр фотографии питомца")
+    public void downloadFotoDog(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        DogsFoto dogsFoto = dogsFotoService.findFotoDog(id);
+        Path path = Path.of(dogsFoto.getFilePath());
+        try (InputStream is = Files.newInputStream(path);
+             OutputStream os = response.getOutputStream();) {
+            response.setStatus(200);
+            response.setContentType(dogsFoto.getMediaType());
+            response.setContentLength((int) dogsFoto.getFileSize());
+            is.transferTo(os);
+        }
+    }
 
 }
--- src/main/java/com/example/pet_shelter/service/DogsFotoService.java
+++ src/main/java/com/example/pet_shelter/service/DogsFotoService.java
@@ -19,47 +19,53 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 public class DogsFotoService {
 
-    @Autowired
-    private DogsService dogsService;
+    private final DogsRepository dogsRepository;
+    private final DogsFotoRepository dogsFotoRepository;
 
-    @Autowired
-    DogsRepository dogsRepository;
+    public DogsFotoService(DogsRepository dogsRepository, DogsFotoRepository dogsFotoRepository) {
+        this.dogsRepository = dogsRepository;
+        this.dogsFotoRepository = dogsFotoRepository;
+    }
 
     @Value("${dir.path.FotoDogs}")
     private String FotoDogsDir;      // Директория фото собак
 
-    public void uploadFotoDog(Long dogId, MultipartFile foto) throws IOException {
-        Optional<Dogs> dogs = dogsService.findDog(dogId);
-        Path filePath = Path.of(FotoDogsDir, dogId + "." + getExtensions(foto.getOriginalFilename()));
+    // Загрузка фото собаки в базу данных и на диск
+    public void uploadFotoDog(Long id, MultipartFile file) throws IOException {
+
+        Dogs dog = dogsRepository.findById(id).orElseThrow();
+        Path filePath = Path.of(FotoDogsDir, id + "." + getExtensions(file.getOriginalFilename()));
         Files.createDirectories(filePath.getParent()); // Создание директории если ее нет
         Files.deleteIfExists(filePath);  // Если такой файл существует, то мы его удаляем
         try (
-                InputStream is = foto.getInputStream();
+                InputStream is = file.getInputStream();
                 OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-                BufferedInputStream bis = new BufferedInputStream(is, 1024);
-                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+                BufferedInputStream bis = new BufferedInputStream(is, 2048);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 2048);
         ) {
             bis.transferTo(bos);
         }
-
-
-//        Dogs dog = findDog(dogId);
-//        DogsFoto dogsFoto = new dogsFoto;
-//        dogsFoto.setDog(dogsFoto);
-//        dogsFoto.setFilePath(filePath.toString());
-//        dogsFoto.setFileSize(foto.getSize());
-//        dogsFoto.setMediaType(foto.getContentType());
-//        dogsFotoRepository.save(dogsFoto);
+        DogsFoto dogsFoto = dogsFotoRepository.findById(id).orElseGet(DogsFoto::new);
+        dogsFoto.setDog(dog);
+        dogsFoto.setFilePath(filePath.toString());
+        dogsFoto.setFileSize(file.getSize());
+        dogsFoto.setMediaType(file.getContentType());
+        dogsFoto.setFotoDog(file.getBytes());
+        dogsFotoRepository.save(dogsFoto);
     }
-
-    public Dogs findDog(Long gogId) {
-        return dogsRepository.findById(gogId).orElseThrow();
-    }
-
 
     private String getExtensions(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
 
+    }
+    // Поиск собаки по id
+    public Dogs findDog(Long gogId) {
+        return dogsRepository.findById(gogId).orElseThrow();
+    }
+
+    // Поиск фото собаки по id
+    public DogsFoto findFotoDog(Long id) {
+        return dogsFotoRepository.findById(id).orElseThrow();
     }
 
 }
