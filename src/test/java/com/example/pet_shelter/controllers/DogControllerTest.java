a1d00ed8e9c2c541b3e86332ce79ca79c23ec907
PDemenkov <guapnik@gmail.com>
2023-02-04 20:09:57 +0300

    Наброски тестов, под любую редакци

---
+++ src/test/java/com/example/pet_shelter/controllers/DogControllerTest.java
@@ -0,0 +1,206 @@
+package com.example.pet_shelter.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.pet_shelter.model.Dogs;
+import com.example.pet_shelter.repository.DogsFotoRepository;
+import com.example.pet_shelter.repository.DogsRepository;
+import com.example.pet_shelter.service.DogsFotoService;
+import com.example.pet_shelter.service.DogsService;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+class DogControllerTest {
+
+    @Test
+    void testGetAllDogs() {
+        DogsRepository dogsRepository = mock(DogsRepository.class);
+        ArrayList<Dogs> dogsList = new ArrayList<>();
+        when(dogsRepository.findAll()).thenReturn(dogsList);
+        DogsService dogsService = new DogsService(dogsRepository);
+        Collection<Dogs> actualAllDogs = (new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)))).getAllDogs();
+        assertSame(dogsList, actualAllDogs);
+        assertTrue(actualAllDogs.isEmpty());
+        verify(dogsRepository).findAll();
+    }
+
+    @Test
+    void testGetAllDogs3() {
+
+        DogsService dogsService = mock(DogsService.class);
+        ArrayList<Dogs> dogsList = new ArrayList<>();
+        when(dogsService.getAllDogs()).thenReturn(dogsList);
+        Collection<Dogs> actualAllDogs = (new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)))).getAllDogs();
+        assertSame(dogsList, actualAllDogs);
+        assertTrue(actualAllDogs.isEmpty());
+        verify(dogsService).getAllDogs();
+    }
+
+
+    @Test
+    void testCreateDog() {
+
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        DogsRepository dogsRepository = mock(DogsRepository.class);
+        when(dogsRepository.save((Dogs) any())).thenReturn(dogs);
+        DogsService dogsService = new DogsService(dogsRepository);
+        DogController dogController = new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)));
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+        assertSame(dogs, dogController.createDog(dogs1));
+        verify(dogsRepository).save((Dogs) any());
+    }
+
+    @Test
+    void testCreateDog3() {
+
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        DogsService dogsService = mock(DogsService.class);
+        when(dogsService.createDog((Dogs) any())).thenReturn(dogs);
+        DogController dogController = new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)));
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+        assertSame(dogs, dogController.createDog(dogs1));
+        verify(dogsService).createDog((Dogs) any());
+    }
+
+
+    @Test
+    void testDeleteDog() {
+
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        DogsRepository dogsRepository = mock(DogsRepository.class);
+        when(dogsRepository.findById((Long) any())).thenReturn(Optional.of(dogs));
+        doNothing().when(dogsRepository).deleteById((Long) any());
+        DogsService dogsService = new DogsService(dogsRepository);
+        assertSame(dogs, (new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)))).deleteDog(11L));
+        verify(dogsRepository).findById((Long) any());
+        verify(dogsRepository).deleteById((Long) any());
+    }
+
+    @Test
+    void testDeleteDog3() {
+   Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        DogsService dogsService = mock(DogsService.class);
+        when(dogsService.deleteDog((Long) any())).thenReturn(dogs);
+        assertSame(dogs, (new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)))).deleteDog(11L));
+        verify(dogsService).deleteDog((Long) any());
+    }
+
+    @Test
+    void testUpdateDog() {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        Optional<Dogs> ofResult = Optional.of(dogs);
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+        DogsRepository dogsRepository = mock(DogsRepository.class);
+        when(dogsRepository.save((Dogs) any())).thenReturn(dogs1);
+        when(dogsRepository.findById((Long) any())).thenReturn(ofResult);
+        DogsService dogsService = new DogsService(dogsRepository);
+        DogController dogController = new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)));
+
+        Dogs dogs2 = new Dogs();
+        dogs2.setAge(1);
+        dogs2.setId(11L);
+        dogs2.setInfoDog("Info Dog");
+        dogs2.setNickname("Sharik");
+        assertSame(dogs1, dogController.updateDog(11L, dogs2));
+        verify(dogsRepository).save((Dogs) any());
+        verify(dogsRepository).findById((Long) any());
+    }
+
+    @Test
+    void testUpdateDog3() {
+
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+        DogsService dogsService = mock(DogsService.class);
+        when(dogsService.updateDog((Long) any(), (Dogs) any())).thenReturn(dogs);
+        DogController dogController = new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)));
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+        assertSame(dogs, dogController.updateDog(11L, dogs1));
+        verify(dogsService).updateDog((Long) any(), (Dogs) any());
+    }
+
+    @Test
+    void testUploadFotoDog() throws IOException {
+
+        DogsService dogsService = new DogsService(mock(DogsRepository.class));
+        DogController dogController = new DogController(dogsService,
+                new DogsFotoService(mock(DogsRepository.class), mock(DogsFotoRepository.class)));
+        ResponseEntity<String> actualUploadFotoDogResult = dogController.uploadFotoDog(11L,
+                new MockMultipartFile("Name", new ByteArrayInputStream("AAAAAAAA".getBytes("UTF-8"))));
+        assertEquals("Файл большого размера", actualUploadFotoDogResult.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, actualUploadFotoDogResult.getStatusCode());
+        assertTrue(actualUploadFotoDogResult.getHeaders().isEmpty());
+    }
+}
+
---
+++ src/test/java/com/example/pet_shelter/controllers/UserControllerTest.java
@@ -0,0 +1,224 @@
+package com.example.pet_shelter.controllers;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+import com.example.pet_shelter.model.Dogs;
+import com.example.pet_shelter.model.Users;
+import com.example.pet_shelter.service.UsersService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ContextConfiguration(classes = {UserController.class})
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+    @Autowired
+    private UserController userController;
+
+    @MockBean
+    private UsersService usersService;
+
+    @Test
+    void testGetAllUsers() throws Exception {
+        when(usersService.getAllUsers()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/user/getALL");
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+
+    @Test
+    void testGetAllUsers2() throws Exception {
+        when(usersService.getAllUsers()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/user/getALL");
+        getResult.characterEncoding("Encoding");
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+
+    @Test
+    void testCreateUser() throws Exception {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+
+        Users users = new Users();
+        users.setDog(dogs);
+        users.setFirstName("Jane");
+        users.setId(11L);
+        users.setLastName("Doe");
+        users.setUserEmail("jane.doe@example.org");
+        users.setUserPhoneNumber("4105551212");
+        when(usersService.createUser((Users) any())).thenReturn(users);
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+
+        Users users1 = new Users();
+        users1.setDog(dogs1);
+        users1.setFirstName("Jane");
+        users1.setId(11L);
+        users1.setLastName("Doe");
+        users1.setUserEmail("jane.doe@example.org");
+        users1.setUserPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(users1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":11,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"userPhoneNumber\":\"4105551212\",\"userEmail\":\"jane.doe"
+                                        + "@example.org\",\"dog\":{\"id\":11,\"nickname\":\"Sharik\",\"age\":1,\"infoDog\":\"Info Dog\"}}"));
+    }
+
+    @Test
+    void testDeleteDog() throws Exception {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+
+        Users users = new Users();
+        users.setDog(dogs);
+        users.setFirstName("Jane");
+        users.setId(11L);
+        users.setLastName("Doe");
+        users.setUserEmail("jane.doe@example.org");
+        users.setUserPhoneNumber("4105551212");
+        when(usersService.deleteUser((Long) any())).thenReturn(users);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/user/delete/{id}", 11L);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":11,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"userPhoneNumber\":\"4105551212\",\"userEmail\":\"jane.doe"
+                                        + "@example.org\",\"dog\":{\"id\":11,\"nickname\":\"Sharik\",\"age\":1,\"infoDog\":\"Info Dog\"}}"));
+    }
+
+    @Test
+    void testDeleteDog2() throws Exception {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+
+        Users users = new Users();
+        users.setDog(dogs);
+        users.setFirstName("Jane");
+        users.setId(11L);
+        users.setLastName("Doe");
+        users.setUserEmail("jane.doe@example.org");
+        users.setUserPhoneNumber("4105551212");
+        when(usersService.updateUser((Long) any(), (Users) any())).thenReturn(users);
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+
+        Users users1 = new Users();
+        users1.setDog(dogs1);
+        users1.setFirstName("Jane");
+        users1.setId(11L);
+        users1.setLastName("Doe");
+        users1.setUserEmail("jane.doe@example.org");
+        users1.setUserPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(users1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/user/update/dog/{id}", 11L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":11,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"userPhoneNumber\":\"4105551212\",\"userEmail\":\"jane.doe"
+                                        + "@example.org\",\"dog\":{\"id\":11,\"nickname\":\"Sharik\",\"age\":1,\"infoDog\":\"Info Dog\"}}"));
+    }
+
+    @Test
+    void testUpdateUsers() throws Exception {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Sharik");
+
+        Users users = new Users();
+        users.setDog(dogs);
+        users.setFirstName("Jane");
+        users.setId(11L);
+        users.setLastName("Doe");
+        users.setUserEmail("jane.doe@example.org");
+        users.setUserPhoneNumber("4105551212");
+        when(usersService.updateUser((Long) any(), (Users) any())).thenReturn(users);
+
+        Dogs dogs1 = new Dogs();
+        dogs1.setAge(1);
+        dogs1.setId(11L);
+        dogs1.setInfoDog("Info Dog");
+        dogs1.setNickname("Sharik");
+
+        Users users1 = new Users();
+        users1.setDog(dogs1);
+        users1.setFirstName("Jane");
+        users1.setId(11L);
+        users1.setLastName("Doe");
+        users1.setUserEmail("jane.doe@example.org");
+        users1.setUserPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(users1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/user/update/{id}", 11L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":11,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"userPhoneNumber\":\"4105551212\",\"userEmail\":\"jane.doe"
+                                        + "@example.org\",\"dog\":{\"id\":11,\"nickname\":\"Sharik\",\"age\":1,\"infoDog\":\"Info Dog\"}}"));
+    }
+}
+
