6ffa22bad38c7f20e4409c0fda88ba801989d84d
93294 <9329496@mail.ru>
2023-02-06 22:47:48 +0300

    Создание сущности ReportUsers

---
+++ src/test/java/com/example/pet_shelter/service/DogsFotoServiceTest.java
@@ -0,0 +1,76 @@
+package com.example.pet_shelter.service;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.pet_shelter.model.Dogs;
+import com.example.pet_shelter.model.DogsFoto;
+import com.example.pet_shelter.repository.DogsFotoRepository;
+import com.example.pet_shelter.repository.DogsRepository;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Disabled;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+@ContextConfiguration(classes = {DogsFotoService.class})
+@ExtendWith(SpringExtension.class)
+class DogsFotoServiceTest {
+    @MockBean
+    private DogsFotoRepository dogsFotoRepository;
+
+    @Autowired
+    private DogsFotoService dogsFotoService;
+
+    @MockBean
+    private DogsRepository dogsRepository;
+
+
+    @Test
+    void testFindDog() {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Bella");
+        Optional<Dogs> ofResult = Optional.of(dogs);
+        when(dogsRepository.findById((Long) any())).thenReturn(ofResult);
+        assertSame(dogs, dogsFotoService.findDog(11L));
+        verify(dogsRepository).findById((Long) any());
+    }
+
+    @Test
+    void testFindFotoDog() {
+        Dogs dogs = new Dogs();
+        dogs.setAge(1);
+        dogs.setId(11L);
+        dogs.setInfoDog("Info Dog");
+        dogs.setNickname("Bella");
+
+        DogsFoto dogsFoto = new DogsFoto();
+        dogsFoto.setDog(dogs);
+        dogsFoto.setFilePath("/directory/foo.txt");
+        dogsFoto.setFileSize(3L);
+        dogsFoto.setId(11L);
+        dogsFoto.setMediaType("Media Type");
+        Optional<DogsFoto> ofResult = Optional.of(dogsFoto);
+        when(dogsFotoRepository.findById((Long) any())).thenReturn(ofResult);
+        assertSame(dogsFoto, dogsFotoService.findFotoDog(11L));
+        verify(dogsFotoRepository).findById((Long) any());
+    }
+}
+
